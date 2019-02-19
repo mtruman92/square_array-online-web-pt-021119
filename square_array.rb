077fb4d97b0a121425f383a48f904250c57bc08d
Mickae Truman <mtruman@nyit.edu>
2019-02-19 00:09:08 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+ new_array = []
+  array.each do |num|new_array << num ** 2
+end
+return new_array 
+end
+
+array = [1,2,3]
+array.collect do|numbers|
+puts numbers ** 2
+numbers ** 2 
 end
